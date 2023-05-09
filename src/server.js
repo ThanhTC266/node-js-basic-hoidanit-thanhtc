1fbd4f273ea78d40215cdfa100693f1026227ba6
ThanhTC266 <93926196+ThanhTC266@users.noreply.github.com>
2023-05-09 17:51:12 +0700

    video7 static files

--- src/server.js
+++ src/server.js
@@ -2,9 +2,11 @@
 // const path = require('path')
 import express from "express";
 import configViewEngine from "./configs/viewEngine";
+require('dotenv').config();
 
 const app = express();
-const port = 3000
+const port = process.env.PORT || 3000;
+console.log('>>> check port: ', port)
 
 configViewEngine(app);
 
